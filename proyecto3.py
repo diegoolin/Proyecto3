77200305a4264a1680efaac940f83293916eb0cc
diegoolin <112409168+diegoolin@users.noreply.github.com>
2022-12-19 00:52:15 -0300

    Add files via upload

--- proyecto3.py
+++ proyecto3.py
@@ -1,3 +1,9 @@
+def contarcodones(codonestotal,p):
+    contador=0
+    for i in codonestotal:
+        if i ==p:
+            contador+=1
+    return contador
 codigog={"GCU":"A","GCC":"A","GCA":"A","GCG":"A","CGU":"R","CGC":"R","CGA":"R","CGG":"R","AGA":"R","AGG":"R",
          "AAU":"N","AAC":"N","GAU":"D","GAC":"D","UGU":"C","UGC":"C","CAA":"Q","CAG":"Q",
          "GAA":"E","GAG":"E","GGU":"G","GGC":"G","GGA":"G","GGG":"G","CAU":"H","CAC":"H",
@@ -5,15 +11,10 @@ codigog={"GCU":"A","GCC":"A","GCA":"A","GCG":"A","CGU":"R","CGC":"R","CGA":"R","
          "AAA":"K","AAG":"K","AUG":"M","UUU":"F","UUC":"F","CCU":"P","CCC":"P","CCA":"P","CCG":"P",
          "UCU":"S","UCC":"S","UCA":"S","UCG":"S","AGU":"S","AGC":"S","ACU":"T","ACC":"T","ACA":"T","ACG":"T",
          "UGG":"W","UAU":"Y","UAC":"Y","GUU":"V","GUC":"V","GUA":"V","GUG":"V"}   
-genetica=open("ejemplo1.fasta")
+genetica=open(input("Ingrese nombre de secuencia:"))
 geneticalimpia=[]
-geneticalimpiastring=""
 nombre=genetica.readlines(1)
 linea=genetica.read()
-
-start=["AUG"]
-stop1=["UAG","UAA","UGA"]
-
 i=0
 while i<len(linea):
     if linea[i]=="I":
@@ -26,8 +27,8 @@ while i<len(linea):
 for j in range(len(geneticalimpia)):
     if geneticalimpia[j]=="T":
         geneticalimpia[j]="U"
-geneticalimpiastring="".join(geneticalimpia)
-print("Sin IF:",geneticalimpiastring)
+geneticalimpia="".join(geneticalimpia)
+print("Sin IF:",geneticalimpia)
 
 start=["AUG"]
 stop1=["UAG","UAA","UGA"]
@@ -35,17 +36,16 @@ largogeneticalimpia=len(geneticalimpia)
 codones=[]
 i=0
 while i<largogeneticalimpia-2:
-    if start[0]==geneticalimpiastring[i:i+3]:
-        while (i+3)<=len(geneticalimpiastring):
-            if stop1[0]==geneticalimpiastring[i:i+3] or stop1[1]==geneticalimpiastring[i:i+3] or stop1[2]==geneticalimpiastring[i:i+3]:
+    if start[0]==geneticalimpia[i:i+3]:
+        while (i+3)<=len(geneticalimpia):
+            if stop1[0]==geneticalimpia[i:i+3] or stop1[1]==geneticalimpia[i:i+3] or stop1[2]==geneticalimpia[i:i+3]:
                 i+=largogeneticalimpia
             else:
 
-                codones.append(geneticalimpiastring[i:i+3])
+                codones.append(geneticalimpia[i:i+3])
                 i+=3
     i+=1
 print("Tripletes:",codones)
-
 cambio=[]
 largo=len(codones)
 i=0
@@ -64,7 +64,7 @@ C=0
 U=0
 G=0
 largo=len(geneticalimpia)
-for i in geneticalimpiastring:
+for i in geneticalimpia:
             if i=="A":
                 A+=1
 
@@ -86,13 +86,9 @@ print("El número de nucleotidos C es de ",C-2," y el porcentaje es:",porcentaje
 print("El número de nucleotidos U es de ",U-2," y el porcentaje es:",porcentajeU)
 print("El número de nucleotidos G es de ",G-2," y el porcentaje es:",porcentajeG)
 
-codonescont=0
-i=0
-while i<(len(codones)):
-    codonescont+=1
-    i+=1
-porcentajecodones=codonescont/100
-print("Hay",codonescont,"codones y el porcentaje es:",porcentajecodones)
+for i in codones:
+    contadorcod=contarcodones(codones,i)
+    print("El codon",i,"se encuentra",contadorcod,"en la secuencia y su porcentaje es:",(contadorcod*100)/len(codones),"%")
 
 polarespositivos=0
 polaresnegativos=0
@@ -116,4 +112,4 @@ for i in cambio:
 print("Aminoácidos polares positivos:",polarespositivos)
 print("Aminoácidos polares negativos:",polaresnegativos)
 print("Aminoácidos polares sin carga:",polaressincarga)
-print("Aminoácidos apolares:",apolares)                               
+print("Aminoácidos apolares:",apolares)
